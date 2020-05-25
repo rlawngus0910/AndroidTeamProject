531797fb262366375eb8a96c5a17ec21b048bb3d
juhyun <rlawngus0910@naver.com>
2020-05-25 10:55:58 +0900

    지역별 전시회 화면 구성

--- app/src/main/java/org/techtown/androidteamproject/LocationExhibition.java
+++ app/src/main/java/org/techtown/androidteamproject/LocationExhibition.java
@@ -2,12 +2,67 @@ package org.techtown.androidteamproject;
 
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class LocationExhibition extends AppCompatActivity {
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+    SeoulExhibition seoulfragment;
+    GyeonggiExhibition gyunggifragment;
+    ChungcheongExhibition chungcheongfragment;
+    GyeongsangExhibition gyeongsangfragment;
+    JeonraExhibition jeonrafragment;
+    GangwonExhibition gangwonfragment;
+    JejuExhibition jejufragment;
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location_exhibition);
+
+        TabLayout location_tab = (TabLayout) findViewById(R.id.location_tabs);
+        seoulfragment = new SeoulExhibition();
+        gyunggifragment = new GyeonggiExhibition();
+        chungcheongfragment = new ChungcheongExhibition();
+        gyeongsangfragment = new GyeongsangExhibition();
+        jeonrafragment = new JeonraExhibition();
+        gangwonfragment = new GangwonExhibition();
+        jejufragment = new JejuExhibition();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, seoulfragment).commit();
+        location_tab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Fragment selected = null;
+                if(position == 0)
+                    selected = seoulfragment;
+                else if(position == 1)
+                    selected = gyunggifragment;
+                else if(position == 2)
+                    selected = chungcheongfragment;
+                else if(position == 3)
+                    selected = gyeongsangfragment;
+                else if(position == 4)
+                    selected = jeonrafragment;
+                else if(position == 5)
+                    selected = gangwonfragment;
+                else if(position == 6)
+                    selected = jejufragment;
+                getSupportFragmentManager().beginTransaction().replace(R.id.container,selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 }
