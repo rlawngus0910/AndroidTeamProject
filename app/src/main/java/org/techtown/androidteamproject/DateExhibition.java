684490080921ace46c2c93c041cedac691e09e8b
KIM <forme3328@naver.com>
2020-05-28 09:26:51 +0900

    date_exhibition(2)

--- app/src/main/java/org/techtown/androidteamproject/DateExhibition.java
+++ app/src/main/java/org/techtown/androidteamproject/DateExhibition.java
@@ -9,5 +9,6 @@ public class DateExhibition extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.date_exhibition);
+        setTitle("날짜별 전시회");
     }
 }
