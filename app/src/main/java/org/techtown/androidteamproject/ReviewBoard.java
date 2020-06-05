02e95a75d1e7f8e4cd2fbf6d4255be9f9277239c
Chiyong-Shin <tlscldyd@gmail.com>
2020-06-05 17:26:56 +0900

    리뷰게시판

--- app/src/main/java/org/techtown/androidteamproject/ReviewBoard.java
+++ app/src/main/java/org/techtown/androidteamproject/ReviewBoard.java
@@ -7,19 +7,19 @@ import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Board extends AppCompatActivity {
+public class ReviewBoard extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.reviewlistboard);
+        setContentView(R.layout.reviewmakingboard);
     }
 
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.reviewbtn:
-                Intent intent = new Intent(getApplicationContext(), ReviewBoard.class);
+            case R.id.goback_btn:
+                Intent intent = new Intent(getApplicationContext(), Board.class);
                 startActivity(intent);
                 break;
-
         }
     }
 }
+
