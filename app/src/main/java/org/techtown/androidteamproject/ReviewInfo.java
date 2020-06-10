0e27b73e4774e5fbd1e89c9611609d2347934eac
ChiyongShin <66486860+ChiyongShin@users.noreply.github.com>
2020-06-10 22:43:06 +0900

    리뷰게시판 작성

---
+++ app/src/main/java/org/techtown/androidteamproject/ReviewInfo.java
@@ -0,0 +1,20 @@
+package org.techtown.androidteamproject;
+
+public class ReviewInfo {
+    private String title;
+    private String content;
+    private String reviewer;
+
+    public ReviewInfo(String title, String content,String reviewer){
+        this.title=title;
+        this.content=content;
+
+    }
+
+    public String getTitle(){ return this.title;}
+    public void setTitle(String title){this.title=title;}
+    public String getContent(){return this.content;}
+    public void setContent(){this.content=content;}
+    public String getReviewer(){return this.reviewer;}
+    public void setReviewer(){this.reviewer=reviewer;}
+}
