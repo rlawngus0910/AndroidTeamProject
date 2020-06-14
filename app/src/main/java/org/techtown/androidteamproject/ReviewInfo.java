be5fbbfe53405a85f6e18bb61aa1e4c436363b46
Chiyong-Shin <tlscldyd@gmail.com>
2020-06-14 17:04:04 +0900

    Review list update

--- app/src/main/java/org/techtown/androidteamproject/ReviewInfo.java
+++ app/src/main/java/org/techtown/androidteamproject/ReviewInfo.java
@@ -5,6 +5,10 @@ public class ReviewInfo {
     private String content;
     private String reviewer;
 
+    public ReviewInfo(){
+
+    }
+
     public ReviewInfo(String title, String content){
         this.title=title;
         this.content=content;
@@ -15,7 +19,7 @@ public class ReviewInfo {
     public String getTitle(){ return this.title;}
     public void setTitle(String title){this.title=title;}
     public String getContent(){return this.content;}
-    public void setContent(){this.content=content;}
+    public void setContent(String content){this.content=content;}
    // public String getReviewer(){return this.reviewer;}
     //public void setReviewer(){this.reviewer=reviewer;}
 }
